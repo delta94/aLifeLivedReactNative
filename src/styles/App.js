20148a6ddd18591c74ed058b222643b8ae0218bf
Max Kelly <max@revelthat.com>
2020-06-08 19:52:53 +1000

    changes

---
+++ src/styles/App.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import {FLEX} from './../styles/styleHelpers';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: FLEX.flexOne,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 30
+  }
+});
+
+export default styles;
---
+++ src/styles/styleHelpers.js
@@ -0,0 +1,7 @@
+export const FLEX = {
+  flexOne: 1
+}
+
+export const COLOR = {
+  grey: '#424242',
+}
