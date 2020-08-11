410ae59442f94121ebcb94888e96e4af3c4529d0
Max Kelly <max@revelthat.com>
2020-08-11 15:18:44 +1000

    Changes

--- src/api/postRequests/createStory.js
+++ src/api/postRequests/createStory.js
@@ -4,7 +4,9 @@ export const createStory = async (storyData) => {
   try {
     const data = await axiosPostWithAuth('/story', {
       storyData
-    })
+    });
+
+    return data;
   } catch (error) {
     console.log(error);
     return {
