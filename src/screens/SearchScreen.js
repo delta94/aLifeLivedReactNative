3069215cdc9f41c3f495a08ad782f25e65108726
Max Kelly <max@revelthat.com>
2020-08-22 18:25:26 +1000

    added to search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import {SearchBar} from 'react-native-elements';
 
 // Styles
 import styles from './../styles/screens/HomeScreen';
 
 const SearchScreen = ({navigation}) => {
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const updateSearch = () => {
+
+  }
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
-        <SearchBar />
+        <SearchBar 
+          placeholder="Try searching for...war stories, sporting achievements"
+          onChangeText={updateSearch}
+          value={searchValue}
+        />
       </View>
     </View>
   );
