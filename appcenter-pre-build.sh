c74d5041b6d43b8b8a666602ea54bd17170d4b35
Max Kelly <max@revelthat.com>
2020-09-03 16:36:20 +1000

    changes

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
+
+# Updating Info.plist
+PLIST=$BUILD_REPOSITORY_LOCALPATH/MyApp/iOS/Info.plist
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString 1.0.${APPCENTER_BUILD_ID}" $PLIST
+
+# Print out file for reference
+cat $PLIST
+
+echo "Updated info.plist!"
+
+# Updating Manifest
+
+MANIFEST="$BUILD_REPOSITORY_LOCALPATH/MyApp/Android/Properties/AndroidManifest.xml"
+
+VERSIONNAME=`grep versionName ${MANIFEST} | sed 's/.*versionName\s*=\s*\"\([^\"]*\)\".*/\1/g'`
+
+sed -i.bak "s/android:versionName="\"${VERSIONNAME}\""/android:versionName="\"1.0.${APPCENTER_BUILD_ID}\""/" ${MANIFEST}
+
+rm -f ${MANIFEST}.bak
+
+# Print out file for reference
+cat $MANIFEST
+
+echo 
+
 # Creates an .env from ENV variables for use with react-native-config
 ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
 printf "Creating an .env file with the following whitelist:\n"
