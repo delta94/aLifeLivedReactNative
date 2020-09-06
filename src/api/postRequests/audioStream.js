a01b59af27302263c72c76888ab02add170b29a4
Max Kelly <max@revelthat.com>
2020-09-06 17:44:01 +1000

    changes

--- src/api/postRequests/audioStream.js
+++ src/api/postRequests/audioStream.js
@@ -173,10 +173,9 @@ export const terminateChannels = async ( questions ) => {
 
 // url mapping is determined by the structure of the audio server
 export const channelIdToUrl = ( channelId ) => {
-  // return `${Config.AUDIO_API_BASE_ROUTE}/channels/${channelId}/${channelId}.wav`;
-  return `https://a-life-lived-audio-server-stag.herokuapp.com/channels/${channelId}/${channelId}.wav`;
-}
+  return `${Config.AUDIO_API_BASE_ROUTE}/channels/${channelId}/${channelId}.wav`;
+};
 
 export const audioFileIdToUrl = ( audioFileId ) => {
-  return `https://a-life-lived-audio-server-stag.herokuapp.com/audio/${audioFileId}.mp3`;
-}
+  return `${Config.AUDIO_API_BASE_ROUTE}/audio/${audioFileId}.mp3`;
+};
